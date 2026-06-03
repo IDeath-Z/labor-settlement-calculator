4f6d0d8cd6f0aa11b4204bb053aae99030486e94
DeathZ <jose.joz46@hotmail.com>
2026-06-03 11:38:38 -0300

    Initial commit: Spring Boot setup and project structure

---
+++ src/test/java/com/deathz/laborcalc/LaborSettlementCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deathz.laborcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborSettlementCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
